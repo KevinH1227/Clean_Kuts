bee8f0a417520f490467a96adbfe00a5ed880724
Kevin Hookumchand <khookumchand@gmail.com>
2021-10-15 18:07:55 -0400

    Users working :) thanks to JA

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :user, only: [ :index, :show, :create, :new, :edit ] do
-    resources :services, :time_slots, :appointments, :favorites, :reviews
-  end
+  # resources :user, only: [ :create, :new, :edit ] do
+  #   resources :services, :time_slots, :appointments, :favorites, :reviews
+  # end
+  # resources :user, only: [ :index, :show ]
   resources :chatroom, only: [ :index, :show ]
   resources :messages, only: [ :index, :create ]
   resources :services, only: [ :index, :show ]
