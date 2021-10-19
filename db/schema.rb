ad82d3ac66e9cdc79c51f009b6c54a673f4c6a18
Jeremy Antoine <jeremincanada@gmail.com>
2021-10-19 12:13:15 -0400

    column post_code in users table more css on banner and search bars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_195753) do
+ActiveRecord::Schema.define(version: 2021_10_19_153856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2021_10_16_195753) do
     t.string "address"
     t.string "phone_number"
     t.integer "role"
+    t.string "post_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
