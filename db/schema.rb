08a44505b4bf79f8ab9928ac5783802e1ea8075c
Kevin Hookumchand <khookumchand@gmail.com>
2021-10-19 19:43:27 -0400

    Added description column to User.barber table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_153856) do
+ActiveRecord::Schema.define(version: 2021_10_19_233715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2021_10_19_153856) do
     t.string "phone_number"
     t.integer "role"
     t.string "post_code"
+    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
