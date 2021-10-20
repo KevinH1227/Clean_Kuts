9b78de2960c5c0874804405d9928d4f4c0a485e6
Kevin Hookumchand <khookumchand@gmail.com>
2021-10-20 14:41:02 -0400

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,19 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
+    authorize @user
+
+    if @user.save
+      redirect_to edit_user_path, notice: "Your profile has been successfully created"
+    else
+      render :new
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :address, :phone_number, :role, :email, :photo)
+    params.require(:user).permit(:first_name, :last_name, :role, :email, :photo)
   end
 end
