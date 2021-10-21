b97acc107e9d6defef8fc3c27962b2e5bad1fbb8
Jeremy Antoine <jeremincanada@gmail.com>
2021-10-21 10:28:20 -0400

    map is not working devise issue??

---
+++ app/javascript/components/map.js
@@ -0,0 +1,41 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 6, duration: 1500 });
+};
+
+  if (mapElement) {
+
+  // map injected into div#map
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+
+  // markers
+  const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+    new mapboxgl.Marker({ "color": "BF9DCF" })
+      .setLngLat([ marker.lng, marker.lat])
+      .setPopup(popup)
+      .addTo(map);
+
+  });
+
+
+fitMapToMarkers(map, markers);
+
+  }
+};
+
+export { initMapbox };
