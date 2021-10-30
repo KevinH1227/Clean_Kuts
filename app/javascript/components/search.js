e2155db8d5f7ef64391fea1dc1c76aacee496ed7
Kristyna Dierstein <kristynahorankova@gmail.com>
2021-10-30 16:24:15 -0400

    search bar done

--- app/javascript/components/search.js
+++ app/javascript/components/search.js
@@ -1,14 +1,14 @@
 const openSearchInput = () => {
-    var containerInput = document.querySelector('.search-container > form > input')
-    var searchIcon = document.querySelector(".search")
-    containerInput.addEventListener('focus', () => {
-        searchIcon.classList.toggle('move-right')
-    })
+  var containerInput = document.querySelector(
+    ".search-container > form > input"
+  );
+  var searchIcon = document.querySelector(".search");
+  containerInput.addEventListener("focus", () => {
+    searchIcon.classList.add("move-right");
+  });
+  containerInput.addEventListener("blur", () => {
+    searchIcon.classList.remove("move-right");
+  });
+};
 
-}
-
-
-
-
-
-export { openSearchInput }
+export { openSearchInput };
