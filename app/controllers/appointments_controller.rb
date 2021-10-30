0f3ffe772ba0bbc896955807688a69716857a1d9
Francesco Perrozzi <francescoperrozzi3@gmail.com>
2021-10-30 12:15:45 -0400

    calendar working

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -10,21 +10,12 @@ class AppointmentsController < ApplicationController
         # Scope your query to the dates being shown:
         start_date = params.fetch(:start_date, Date.today).to_date
         @time_slots = @barber.time_slots
-    # For a monthly view:
-        # @appointments = Appointment.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-    
-    # Or, for a weekly view:
         @appointments = Appointment.where(start_time: start_date.beginning_of_week..start_date.end_of_week)
     end
 
     def index
-# Scope your query to the dates being shown:
+    # Scope your query to the dates being shown:
     start_date = params.fetch(:start_date, Date.today).to_date
-
-# For a monthly view:
-    @appointments = Appointment.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-
-# Or, for a weekly view:
     @appointments = Appointment.where(starts_at: start_date.beginning_of_week..start_date.end_of_week)
         # @my_client_appointments = current_user.appointments
         # raise
