9a11cb60e84ce627d679060d195ee552f35eaf4e
Jeremy Antoine <jeremincanada@gmail.com>
2021-10-31 01:20:49 -0400

    map is working only when you re signed out..

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,19 +4,20 @@ class PagesController < ApplicationController
   def home
     @users = User.all
     @address = current_user.nil? ? "Montreal" : current_user.address
-    if User.barber.size > 4
-      @barbers = User.near(@address, 1000).where(role: User.roles[:barber]).first(5)
-    else
-      @barbers = User.where(role: "barber")
-    end
+    # if User.barber.size > 4
+    #   @barbers = User.near(@address, 1000).where(role: User.roles[:barber]).first(5)
+      @barbers = User.where(role:'barber').near(@address, 100)
+    # else
+    #   @barbers = User.where(role: "barber")
+    # end
     # @barbers = User.where(role: "barber")
     # @barber = User.find(params[:id])
-    # @markers = @barbers.geocoded.map do |barber|
-    #   {
-    #     lat: barber.latitude,
-    #     lng: barber.longitude,
-    #     info_window: render_to_string(partial: "info_window", locals: { barber: barber })
-    #   }
-    # end
+    @markers = @barbers.geocoded.map do |barber|
+      {
+        lat: barber.latitude,
+        lng: barber.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { barber: barber })
+      }
+    end
   end
 end
