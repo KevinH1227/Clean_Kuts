e3f718cee5935263b3004e09e2eec27a50611bb6
Jeremy Boxerman <jboxerman08@gmail.com>
2021-11-02 20:05:16 -0400

    added styling to services card (WIP)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,12 @@ class PagesController < ApplicationController
   def home
     @users = User.all
     @address = current_user.nil? ? "Montreal" : current_user.address
-    if User.barber.size > 5
-      @barbers = User.near(@address, 500).where(role: User.roles[:barber]).first(5)
-    else
-      @barbers = User.near(@address, 500).where(role: User.roles[:barber])
-    end
-    # @barbers = User.where(role: "barber")
+    # if User.barber.size > 5
+    #   @barbers = User.near(@address, 500).where(role: User.roles[:barber]).first(5)
+    # else
+    #   @barbers = User.near(@address, 500).where(role: User.roles[:barber])
+    # end
+    @barbers = User.where(role: "barber")
     # @barber = User.find(params[:id])
     @markers = @barbers.geocoded.map do |barber|
       {
